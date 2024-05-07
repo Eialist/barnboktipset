6fcb255c184cb28825f87f66e91633dced61c9b3
Eialist <elsinoree@gmail.com>
2024-05-07 15:14:41 +0200

    fixed dirname

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ import path from 'path';
 
 dotenv.config();
 const app = express();
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 app.use(express.static(path.join(__dirname, "dist")));
 
 const PORT = process.env.PORT || 3000;
