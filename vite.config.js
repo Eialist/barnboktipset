83792d0f401f69f1a6cc3924ebafbfc8b9ed6fe3
Eialist <elsinoree@gmail.com>
2024-05-07 15:40:04 +0200

    changed target in proxy

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://barnboktipset.onrender.com",
+        target: "http://localhost:6789",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
       }
